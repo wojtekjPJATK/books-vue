c072ef42fe02f9e0784ebab31d719691c74389e7
Wojtek J <wojtekj1989@gmail.com>
2018-12-16 17:16:28 +0100

    O auth (#2)
    * added OAuth
    
    * refactor

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import "./plugins/vuetify";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import GoogleAuth from "vue-google-oauth";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(GoogleAuth, {
+  client_id:
+    "119036505724-l7aourh8dss0mvb0jp7bfr951l8sc8r4.apps.googleusercontent.com"
+});
+Vue.googleAuth().load();
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
---
+++ src/views/Auth.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-layout align-center column class="mx-4">
+    <h1>Auth</h1>
+  </v-layout>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
